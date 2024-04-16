62e28487ee1f9044d8304fc83b3bc021433e1103
MrMoodles123 <mahir12moodaley@gmail.com>
2024-04-16 14:02:32 +0200

    First commit. Added classifier.py that has main method that continously takes in input for a file name.

---
+++ classifier.py
@@ -0,0 +1,16 @@
+# CSC3022F ML A1
+# Mahir Moodaley (MDLMAH007)
+# 11 April 2024
+
+def read_file(filename):
+    return
+
+def main():
+    while True:
+        input_str = input("Please enter a filepath:\n> ")
+        if input_str.strip().lower() == "exit":
+            print("Exiting...")
+            break
+        
+if __name__ == "__main__":
+    main()
